6534098e9bbbb8c3ff530fafaf13b3a96df12e64
Mengyao512 <2145559681@qq.com>
2019-06-27 11:30:52 +0800

    Mybatis

---
+++ src/main/java/com/bdqn/entity/Student.java
@@ -0,0 +1,39 @@
+package com.bdqn.entity;
+
+public class Student {
+    private int id;
+    private String stu_name;
+    private int stu_age;
+    private String stu_address;
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getStu_name() {
+        return stu_name;
+    }
+
+    public void setStu_name(String stu_name) {
+        this.stu_name = stu_name;
+    }
+
+    public int getStu_age() {
+        return stu_age;
+    }
+
+    public void setStu_age(int stu_age) {
+        this.stu_age = stu_age;
+    }
+
+    public String getStu_address() {
+        return stu_address;
+    }
+
+    public void setStu_address(String stu_address) {
+        this.stu_address = stu_address;
+    }
+}
